ec2069302ab478a05e072f28e518fffb15caad0b
michael <michaems@outlook.com>
2019-03-24 22:00:37 +0200

    Embeded env. added

---
+++ SetCrossEnv.bat
@@ -0,0 +1,4 @@
+export PATH=/home/michael/learningDeviceDriver/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin:$PATH
+export TOOLCHAIN=/home/michael/learningDeviceDriver/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export ARCH=arm
